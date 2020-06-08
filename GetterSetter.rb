a744bd2ecefc19071f1e14ca9e76d4dc53e4c940
chirchir <chirchir7370@gmail.com>
2020-06-08 17:23:27 +0300

    add setter and getter

---
+++ GetterSetter.rb
@@ -0,0 +1,16 @@
+class SetterGetter
+    def initialize(name)
+        @name=name
+    end
+
+    def get_name
+        @name
+        
+    end
+    def set_name=(name)
+        @name=name
+        
+    end
+    
+end
+
